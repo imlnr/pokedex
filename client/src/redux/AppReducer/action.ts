ad6561f14a59623a71c6bd2405c9b286a6b713f3
imlnr <imlnr2002@gmail.com>
2025-06-08 01:51:13 +0530

    updated the middleware in pokemon api and handelled the logout state

--- client/src/redux/AppReducer/action.ts
+++ client/src/redux/AppReducer/action.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 import type { Dispatch } from "redux";
-import { GET_LOADING_FALSE, GET_LOADING_TRUE, GET_POKEMON_DATA_FAILURE, GET_POKEMON_DATA_REQUEST, GET_POKEMON_DATA_SUCCESS, GET_SINGLE_POKEMON_FAILURE, GET_SINGLE_POKEMON_REQUEST, GET_SINGLE_POKEMON_SUCCESS, GET_USER_FAILURE, GET_USER_REQUEST, GET_USER_SUCCESS } from "./action-types";
+import { GET_LOADING_FALSE, GET_LOADING_TRUE, GET_LOGOUT_USER, GET_POKEMON_DATA_FAILURE, GET_POKEMON_DATA_REQUEST, GET_POKEMON_DATA_SUCCESS, GET_SINGLE_POKEMON_FAILURE, GET_SINGLE_POKEMON_REQUEST, GET_SINGLE_POKEMON_SUCCESS, GET_USER_FAILURE, GET_USER_REQUEST, GET_USER_SUCCESS } from "./action-types";
 
 const url = import.meta.env.VITE_BACKEND_URL || "";
 
@@ -28,6 +28,11 @@ export const getCurrentUserData = () => async (dispatch: Dispatch) => {
         return responseData
     } catch (error) {
         dispatch({ type: GET_USER_FAILURE })
+        Cookies.remove('accessToken');
+        Cookies.remove("isLoggedIn")
+        dispatch({ type: GET_LOGOUT_USER })
+        // Redirect to login page
+        window.location.href = '/login';
     }
 }
 
--- server/routes/pokemon.routes.ts
+++ server/routes/pokemon.routes.ts
@@ -3,8 +3,8 @@ import express from "express"
 import { fetAllPokemon, getAllPokemons, getSinglePokemon } from "../controllers/pokemon.controllers";
 import { verifyGoogleToken } from "../middlewares/auth.middleware";
 const PokemonRouter = express.Router();
-PokemonRouter.get('/all/paginated', fetAllPokemon);
-PokemonRouter.get('/:id', getSinglePokemon);
-PokemonRouter.get('/search/all', getAllPokemons);
+PokemonRouter.get('/all/paginated', verifyGoogleToken, fetAllPokemon);
+PokemonRouter.get('/:id', verifyGoogleToken, getSinglePokemon);
+PokemonRouter.get('/search/all', verifyGoogleToken, getAllPokemons);
 
 export default PokemonRouter
